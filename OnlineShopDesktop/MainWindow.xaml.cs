d82c4c49322a5b45b8091d405275736dd5d2762c
Anastasiya Levina <miss.levinaayandex.ru>
2023-06-27 12:02:59 +0700

    OnlineShop_ADO_WPF_added

---
+++ OnlineShopDesktop/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Data;
+using System.Windows;
+using System.Data.SqlClient;
+using System.Windows.Controls;
+
+namespace OnlineShopDesktop
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private SqlConnection _sqlconnection;
+        private SqlDataAdapter _sqlDataAdapter;
+        private DataTable _dataTable;
+        private DataRowView _dateRowView;
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void GridView_OnCurrentCellChanged(object? sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void GridView_OnCellEditEnding(object? sender, DataGridCellEditEndingEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
